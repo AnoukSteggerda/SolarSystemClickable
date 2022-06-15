cbe575e2e38d094c3fceead09825713262a565ca
AnoukSteggerda <as.steggerda@gmail.com>
2022-06-15 16:19:05 +0200

    js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
-// document
-//     .querySelector("#neptune")
-//     .addEventListener("mouseenter", e => {
-//       location.href = "neptune.html";
-//     });
+AFRAME.registerComponent('thumbstick-logging',{
+    init: function () {
+      this.el.addEventListener('thumbstickmoved', this.logThumbstick);
+    },
+    logThumbstick: function (evt) {
+      if (evt.detail.y > 0.95) { console.log("DOWN"); }
+      if (evt.detail.y < -0.95) { console.log("UP"); }
+      if (evt.detail.x < -0.95) { console.log("LEFT"); }
+      if (evt.detail.x > 0.95) { console.log("RIGHT"); }
+    }
+  });
